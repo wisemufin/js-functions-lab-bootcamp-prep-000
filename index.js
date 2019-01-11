898c8acff2c1e17a4f780c8b1ce93b9204489a0d
Matthew Trabold <mct4240@gmail.com>
2019-01-11 22:24:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return (`Happy ${holiday}, ${name}!`)
 }
 
-function holidayCoundown(holiday, days) {
+function holidayCountdown(holiday, days) {
   return (`It's ${days} days until ${holiday}!`)
 }
